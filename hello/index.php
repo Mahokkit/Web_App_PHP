f4573f467ac715a6399a82b9cec0c5f540e840d7
Mahokkit <ma.edward@hotmail.com>
2017-09-11 16:26:54 -0300

    updating for phpstorm

---
+++ hello/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Edward Ma
+ * Date: 9/11/17
+ * Time: 13:38
+ */
+$number1 = 5;
+$number2 = 8;
+$sum = $number1 + $number2;
+echo "<h1>The sum is: ";
+echo $sum;
+echo "</h1>";
+
+?>
+
+
+
