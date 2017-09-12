96c6abf05c6de68e425ebda45dc2a74e47d9c493
Mahokkit <ma.edward@hotmail.com>
2017-09-12 20:28:27 -0300

    updating for phpstorm

--- LabOnePartB/index.php
+++ LabOnePartB/index.php
@@ -6,9 +6,12 @@
  * Time: 8:11 AM
  */
 
-$message = "<h1>Greeting From Lab One</h1>";
-
+$name = "Edward Ma";
+$blastoff = array(10,9,8,7,6,5,4,3,2,1,"blast off!");
+$colours = array("Red","Orange","Teal","Blue","Green","Purple","Grey");
 ?>
+
+
 
 <html>
     <header>
@@ -16,6 +19,23 @@ $message = "<h1>Greeting From Lab One</h1>";
     </header>
 
     <body>
-        <p><?php echo $message ?></p>
+        <p>
+            <?php
+            echo "<h1>Greeting From Lab One</h1> \n" . "My name is " . $name;
+            echo "<br>Answer to Step 4 A is " . ((32*14)+83) . "!" .
+                "<br>Answer to Step 4 B is " . ((1024/128)-7) . "!" .
+                "<br>Answer of Step 4 C is " . (769%6). "!";
+            ?>
+        </p>
+        <p>
+            <h3>Step 5 & 6...loooops!</h3>
+            <?php
+                foreach ($blastoff as $value)
+                {
+                    echo "$value <br>";
+                }
+            ?>
+        </p>
+
     </body>
 </html>
