d06d89ccfc9085f8fc7bf7fe99ba63fe0a856d10
Mahokkit <ma.edward@hotmail.com>
2017-09-12 08:18:47 -0300

    updating for phpstorm

---
+++ LabOnePartB/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Edward Ma
+ * Date: 9/12/17
+ * Time: 8:11 AM
+ */
+
+$message = "<h1>Greeting From Lab One</h1>";
+
+?>
+
+<html>
+    <header>
+        <title>Lab One Part B</title>
+    </header>
+
+    <body>
+        <p><?php echo $message ?></p>
+    </body>
+</html>
