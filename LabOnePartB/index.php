b7a05a10ed09dfeef13ea2e0924969322f212e42
Mahokkit <ma.edward@hotmail.com>
2017-09-13 11:08:13 -0300

    updating for phpstorm

--- LabOnePartB/index.php
+++ LabOnePartB/index.php
@@ -7,8 +7,8 @@
  */
 
 $name = "Edward Ma";
-$blastoff = array(10,9,8,7,6,5,4,3,2,1,"blast off!");
-$colours = array("Red","Orange","Teal","Blue","Green","Purple","Grey");
+$blastoff = array(10,9,8,7,6,5,4,3,2,1,"blast off!"); //For Step 5
+$colours = array("Red","Orange","Teal","Blue","Green","Purple","Grey"); //For Step 6
 ?>
 
 
@@ -20,7 +20,7 @@ $colours = array("Red","Orange","Teal","Blue","Green","Purple","Grey");
 
     <body>
         <p>
-            <?php
+            <?php //Step 1 to 4
             echo "<h1>Greeting From Lab One</h1> \n" . "My name is " . $name;
             echo "<br>Answer to Step 4 A is " . ((32*14)+83) . "!" .
                 "<br>Answer to Step 4 B is " . ((1024/128)-7) . "!" .
@@ -33,6 +33,33 @@ $colours = array("Red","Orange","Teal","Blue","Green","Purple","Grey");
                 foreach ($blastoff as $value)
                 {
                     echo "$value <br>";
+                }
+            ?>
+        </p>
+        <p>
+            <b> Step 6 2.a </b><br>
+            <?php
+                for ($x = 0; $x <=6; $x++)
+                {
+                    echo "Colour Number " . ($x+1) . " is " . $colours[$x] . "<br>";
+                }
+                echo "<br><b>Step 6 2.b</b><br>";
+                $y = 0;
+                foreach ($colours as $values)
+                {
+                    $y = $y+1;
+                    echo "Colour Number " . ($y) . " is " . $values . "<br>";
+                }
+            ?>
+        </p>
+        <p>
+            <b>Backward loops!</b><br>
+            <?php
+                $y = 8;
+                foreach (array_reverse($colours) as $value)
+                {
+                    $y = $y - 1;
+                    echo "Colour Number " . ($y) . " is " . $value . "<br>";
                 }
             ?>
         </p>
