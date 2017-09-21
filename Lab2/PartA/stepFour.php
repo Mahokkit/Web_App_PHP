ca2742e2459dfeb5c9e12126ed451383cd6824b8
Mahokkit <ma.edward@hotmail.com>
2017-09-21 16:18:43 -0300

    updating for phpstorm

---
+++ Lab2/PartA/stepFour.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Lab 2 Part A Step 4</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>This page was rendered in PHP version " . phpversion() . "</h1><br>";
+            echo "<h2>This page was rendered in ZEND version " . zend_version() . "</h2><br>";
+            echo "<h3>"
+        ?>
+    </body>
+</html>
