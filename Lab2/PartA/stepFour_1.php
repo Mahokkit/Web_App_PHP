0da758033b9440103bbb06bee98e353e77d1a847
Mahokkit <ma.edward@hotmail.com>
2017-09-27 07:58:21 -0300

    updating for phpstorm

---
+++ Lab2/PartA/stepFour_1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Information</title>
+    </head>
+    <body>
+        <?php
+            echo phpinfo();
+        ?>
+    </body>
+</html>
