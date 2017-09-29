9cf68f4257010f201b0de0c247d92ae6c20520bb
Mahokkit <ma.edward@hotmail.com>
2017-09-28 21:06:24 -0300

    updating for phpstorm

--- Lab3/PartC/validation.js
+++ Lab3/PartC/validation.js
@@ -39,43 +39,78 @@ function change()
     var adOne = document.getElementById('addressOne');
     var email = document.getElementById('email');
 
+
     if(fName.value == '')
     {
         fName.style.borderColor = 'red';
+        fName.style.background = "";
     }
     else
     {
         fName.style.borderColor = '';
         fName.style.background = 'yellow';
+
     }
+
 
     if(lName.value == '')
     {
         lName.style.borderColor = 'red';
+        lName.style.background = "";
     }
     else
     {
         lName.style.borderColor = '';
         lName.style.background = 'yellow';
+
     }
 
     if(adOne.value == '')
     {
         adOne.style.borderColor = 'red';
+        adOne.style.background = "";
     }
     else
     {
         adOne.style.borderColor = '';
         adOne.style.background = 'yellow';
+
     }
 
     if(email.value == '')
     {
         email.style.borderColor = 'red';
+        email.style.background = "";
     }
     else
     {
         email.style.borderColor = '';
         email.style.background = 'yellow';
+
     }
+}
+
+function filling(a)
+{
+    var txtFName = document.getElementById('fName');
+    var txtLName = document.getElementById('lName');
+    var txtAddOne = document.getElementById('addOne');
+    var txtEmail = document.getElementById('email2')
+    if (a == 'fName')
+    {
+        txtFName.style.textDecoration = 'underline';
+    }
+    else if (a == 'lName')
+    {
+        txtLName.style.textDecoration = 'underline';
+    }
+    else if (a == 'addOne')
+    {
+        txtAddOne.style.textDecoration = 'underline';
+    }
+    else if (a == 'email2')
+    {
+        txtEmail.style.textDecoration = 'underline';
+    }
+
 }
