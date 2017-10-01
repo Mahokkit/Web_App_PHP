a4f507394b4023171e0db51b0b4c164e8abe9bc5
Mahokkit <ma.edward@hotmail.com>
2017-10-01 20:28:58 -0300

    updating for phpstorm

--- Assignment1/validation.js
+++ Assignment1/validation.js
@@ -4,6 +4,10 @@ function validate()
     {
         return false;
     }
+    else if (chkDate() == false)
+    {
+        return false;
+    }
     else
     {
         return true;
@@ -31,5 +35,19 @@ function chkName()
 
 function chkDate()
 {
+    var dateChk = /^[0-9]{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/;
+    // http://www.webdeveloper.com/forum/showthread.php?190078-Javascript-Date-(yyyy-mm-dd)-validation
+    var BDate = document.getElementById('empBDate');
+    var HDate = document.getElementById('empHDate');
 
+    if (!dateChk.test(BDate.value))
+    {
+        alert("Date must be in YYYY-MM-DD Format")
+        return false;
+    }
+    else if (!dateChk.test(HDate.value))
+    {
+        alert("Date Must be in YYYY-MM-DD Format")
+        return false;
+    }
 }
--- Lab3/PartB/updateActorForm.php
+++ Lab3/PartB/updateActorForm.php
@@ -1,41 +1,36 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Updating Actors</title>
-    </head>
-    <body>
-    <?php
+<head>
+    <title>Updating Actors</title>
+</head>
+<body>
+<?php
+require_once("dbConn.php");
+$conn = getDbConnection();
+if(!$conn)
+{
+    die("Unable to connect to database: " . mysqli_connect_error());
+}
+$actorId = $_POST['actorIdUpdate'];
+$sql = "SELECT first_name,last_name FROM actor WHERE actor_id = $actorId;";
+$result = mysqli_query($conn, $sql);
+if(!$result)
+{
+    die("Actor with id $actorId was not found");
+}
+else
+{
+    $data = mysqli_fetch_assoc($result);
+    $firstName = $data['first_name'];
+    $lastName = $data['last_name'];
+}
+?>
+<form id="updateActor" name="updateActor" method="post" action="updateActor.php">
+    <p><label>Actor Id: <input type="text" name="actorId" id="actorId" value="<?php echo $actorId; ?>" readonly/></label></p>
+    <p><label>First Name: <input type="text" name="firstName" id="firstName" value="<?php echo $firstName; ?>" /></label></p>
+    <p><label>Last Name: <input type="text" name="lastName" id="lastName" value="<?php echo $lastName; ?>" /></label></p>
+    <p><input type="submit" id="submit" value="Update Actor" /></p>
+</form>
 
-    require_once("dbConn.php");
-    $conn = getDbConnection();
-    if(!$conn)
-    {
-        die("Unable to connect to database: " . mysqli_connect_error());
-    }
-
-    $actorId = $_POST['actorIdUpdate'];
-
-
-    $sql = "SELECT first_name,last_name FROM actor WHERE actor_id = $actorId;";
-    $result = mysqli_query($conn, $sql);
-    if(!$result)
-    {
-        die("Actor with id $actorId was not found");
-    }
-    else
-    {
-        $data = mysqli_fetch_assoc($result);
-        $firstName = $data['first_name'];
-        $lastName = $data['last_name'];
-    }
-
-    ?>
-        <form id="updateActor" name="updateActor" method="post" action="updateActor.php">
-            <p><label>Actor Id: <input type="text" name="actorId" id="actorId" value="<?php echo $actorId; ?>" /></label></p>
-            <p><label>First Name: <input type="text" name="firstName" id="firstName" value="<?php echo $firstName; ?>" /></label></p>
-            <p><label>Last Name: <input type="text" name="lastName" id="lastName" value="<?php echo $lastName; ?>" /></label></p>
-            <p><input type="submit" id="submit" value="Update Actor" /></p>
-        </form>
-
-    </body>
+</body>
 </html>
