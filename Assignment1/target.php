3aee2849f98eb86308aea6f98e1bf2a9838e5c43
Mahokkit <ma.edward@hotmail.com>
2017-10-01 13:33:13 -0300

    updating for phpstorm

--- Assignment1/target.php
+++ Assignment1/target.php
@@ -5,6 +5,19 @@
     <title>Target Page</title>
 </head>
 <body>
+    <?php
+        $empFName = $_POST['empFName'];
+        $empLName = $_POST['empLName'];
+        $empBDate = $_POST['empBDate'];
+        $empGender = $_POST['empGender'];
+        $empHDate = $_POST['empHDate'];
+
+        echo
+            "<p>Employee Name: " . $empFName . " " . $empLName . "<br>" .
+            "Employee Birth Date: " . $empBDate . "<br>" .
+            "Employee Gender: " . $empGender . "<br>" .
+            "Employee Hired Date: " . $empHDate . "</p>";
+    ?>
     <p><h1>SUCCESS!</h1></p>
 </body>
 </html>
