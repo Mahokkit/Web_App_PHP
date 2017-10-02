fc1e1afd5a0163eafaf32cbd5ae1990edca77e8e
Mahokkit <ma.edward@hotmail.com>
2017-10-02 08:31:16 -0300

    updating for phpstorm

--- Assignment1/target.php
+++ Assignment1/target.php
@@ -6,11 +6,28 @@
 </head>
 <body>
     <?php
+
+        require_once("dbConn.php");
+        $conn = getDbConnection();
+
         $empFName = $_POST['empFName'];
         $empLName = $_POST['empLName'];
         $empBDate = $_POST['empBDate'];
         $empGender = $_POST['empGender'];
         $empHDate = $_POST['empHDate'];
+
+        $sql = "INSERT INTO employees (birth_date, first_name, last_name, gender, hire_date) VALUE ('$empBDate','$empFName', '$empLName','$empGender','$empHDate')";
+
+        $result = mysqli_query($conn, $sql);
+        $affected = mysqli_affected_rows($conn);
+        if(!$result)
+        {
+            die("Unable to update record: " . mysqli_error($conn));
+        }
+        else
+        {
+            echo "<h2>Success! " . $affected . " record was updated.</h2>";
+        }
 
         echo
             "<p>Employee Name: " . $empFName . " " . $empLName . "<br>" .
