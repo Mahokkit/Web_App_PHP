0d14581edd9af9bb58063d06eb411d52e1aa0ca5
Mahokkit <ma.edward@hotmail.com>
2017-10-04 15:10:21 -0300

    updating for phpstorm

--- Assignment1/employees.php
+++ Assignment1/employees.php
@@ -1,112 +1,100 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Employees Table</title>
-        <style>
-            table, th, tr, td
-            {
-                border: solid 1px maroon;
-                text-align: center;
-            }
-        </style>
-        <script src="validation.js" type="text/javascript"></script>
-    </head>
-    <body>
-        <p><b>Search First & Last Name From Database</b></p>
-        <form method="post">
-            <p><label>Search: <input type="text" name="nSearch" value="<?php echo $_POST['nSearch']; ?>"></label></p>
-            <p><input type="submit" value="Submit Query"></p>
-        </form>
-        <table>
-            <thead>
-            <th>Employee #</th>
-            <th>Date of Birth</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Gender</th>
-            <th>Date of Hire</th>
-            </thead>
-            <tbody>
-            <?php
-            require_once("dbConn.php");
-            $conn = getDbConnection();
-
-
-            if(isset($_GET['id'])){
-                $rowStarts = $_GET['id'];
-            }else{
-                $rowStarts = 0;
-            }
-
-            $maxRow = 25;
-
-            $TotalRec = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM 'employees'"));
-
-            $sql = "SELECT * FROM employees LIMIT $rowStarts, $maxRow";
-            $str = (string)$_POST['nSearch'];
-            $searchSQL = "SELECT * FROM employees WHERE first_name LIKE '%$str%' OR last_name LIKE '%$str%' LIMIT $rowStarts,$maxRow";
-
-            if ($str = "")
-            {
-                $result = mysqli_query($conn,$sql);
-            }
-            else
-            {
-                $result = mysqli_query($conn,$searchSQL);
-            }
-
-            $affected = mysqli_affected_rows($conn);
-
-            if(!$result)
-            {
-                die("Could not retrieve records from database: " . mysqli_error($conn));
-            }
-
-            while($row = mysqli_fetch_assoc($result)):
-                ?>
-                <tr>
-                    <td><?php echo $row['emp_no'] ?></td>
-                    <td><?php echo $row['birth_date'] ?></td>
-                    <td><?php echo $row['first_name'] ?></td>
-                    <td><?php echo $row['last_name'] ?></td>
-                    <td><?php echo $row['gender'] ?></td>
-                    <td><?php echo $row['hire_date'] ?></td>
-                    <td><img src="delete.png" width="25" height="25" /> </td>
-                    <td><img src="edit.png" width="25" height="25" /></td>
-                </tr>
-
-                <?php
-            endwhile;
-
-            mysqli_close($conn);
-            ?>
-            </tbody>
-        </table>
-        <?php
-        if($rowStarts == 0)
+<head>
+    <meta charset="UTF-8">
+    <title>Employees Table</title>
+    <style>
+        table, th, tr, td
         {
-//            echo "Previous &laquo;";
-            echo "<button> &lt; </button>";
+            border: solid 1px maroon;
+            text-align: center;
         }
-        else
-        {
-            echo "<a href='employees.php?id=" . ($rowStarts - 25) ."'><button>&lt;</button></a>";
-        }
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-
-        if($rowStarts == $TotalRec-1)
-        {
-            echo "<button>&gt;</button>";
-        }
-        else
-        {
-            echo "<a href='employees.php?id=" . ($rowStarts + 25) ."'><button>&gt;</button></a>";
-        }
-//        Source: https://www.sitepoint.com/community/t/previous-and-next-row-from-mysql-using-php/3354/15
-
+    </style>
+    <script src="validation.js" type="text/javascript"></script>
+</head>
+<body>
+<p><b>Search First & Last Name From Database</b></p>
+<form method="post">
+    <p><label>Search: <input type="text" name="nSearch" value="<?php echo $_POST['nSearch']; ?>"></label></p>
+    <p><input type="submit" value="Submit Query"></p>
+</form>
+<table>
+    <thead>
+    <th>Employee #</th>
+    <th>Date of Birth</th>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Gender</th>
+    <th>Date of Hire</th>
+    </thead>
+    <tbody>
+    <?php
+    require_once("dbConn.php");
+    $conn = getDbConnection();
+    if(isset($_GET['id'])){
+        $rowStarts = $_GET['id'];
+    }else{
+        $rowStarts = 0;
+    }
+    $maxRow = 25;
+    $TotalRec = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM 'employees'"));
+    $sql = "SELECT * FROM employees LIMIT $rowStarts, $maxRow";
+    $str = (string)$_POST['nSearch'];
+    $searchSQL = "SELECT * FROM employees WHERE first_name LIKE '%$str%' OR last_name LIKE '%$str%' LIMIT $rowStarts,$maxRow";
+    if ($str = "")
+    {
+        $result = mysqli_query($conn,$sql);
+    }
+    else
+    {
+        $result = mysqli_query($conn,$searchSQL);
+    }
+    $affected = mysqli_affected_rows($conn);
+    if(!$result)
+    {
+        die("Could not retrieve records from database: " . mysqli_error($conn));
+    }
+    while($row = mysqli_fetch_assoc($result)):
         ?>
-    </body>
+        <tr>
+            <td><?php echo $row['emp_no'] ?></td>
+            <td><?php echo $row['birth_date'] ?></td>
+            <td><?php echo $row['first_name'] ?></td>
+            <td><?php echo $row['last_name'] ?></td>
+            <td><?php echo $row['gender'] ?></td>
+            <td><?php echo $row['hire_date'] ?></td>
+            <td><img src="delete.png" width="25" height="25" /> </td>
+            <td><img src="edit.png" width="25" height="25" /></td>
+        </tr>
+
+        <?php
+    endwhile;
+    mysqli_close($conn);
+    ?>
+    </tbody>
+</table>
+<?php
+if($rowStarts == 0)
+{
+//            echo "Previous &laquo;";
+    echo "<button> &lt; </button>";
+}
+else
+{
+    echo "<a href='employees.php?id=" . ($rowStarts - 25) ."'><button>&lt;</button></a>";
+}
+echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+if($rowStarts == $TotalRec-1)
+{
+    echo "<button>&gt;</button>";
+}
+else
+{
+    echo "<a href='employees.php?id=" . ($rowStarts + 25) ."'><button>&gt;</button></a>";
+}
+//        Source: https://www.sitepoint.com/community/t/previous-and-next-row-from-mysql-using-php/3354/15
+?>
+</body>
 </html>
 
 <!--SELECT * FROM (SELECT * FROM employees ORDER BY emp_no DESC LIMIT 25) sub ORDER BY emp_no ASC;-->
