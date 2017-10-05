4db9d177d61b528cef3145cb37fcfeb2b07334e6
Mahokkit <ma.edward@hotmail.com>
2017-10-05 10:00:06 -0300

    updating for phpstorm

--- Assignment1/mainLogin.php
+++ Assignment1/mainLogin.php
@@ -7,7 +7,21 @@
     <body>
         <h1>Member Login</h1>
         <form name="form1" method="post" action="checkLogin.php">
-            
+            <p>
+                <label for="loginUser"> User: </label>
+            </p>
+            <p>
+                <input name="loginUser" type="text" id="loginUser" />
+            </p>
+            <p>
+                <label for="loginPwd">Password: </label>
+            </p>
+            <p>
+                <input name="loginPwd" type="password" id="loginPwd" />
+            </p>
+            <p>
+                <input type="submit" name="Submit" value="Login"/>
+            </p>
         </form>
         <?php
 
