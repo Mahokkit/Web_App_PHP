84295115d54f0776e99bd84f84676ed453de9e71
Mahokkit <ma.edward@hotmail.com>
2017-10-06 18:44:27 -0300

    updating for phpstorm

--- Assignment1/page1.php
+++ Assignment1/page1.php
@@ -10,6 +10,7 @@
         <title>SELECT</title>
     </head>
     <body>
+        <a href="form.php">Add Employees</a>
         <a href="employees.php"> Search for Employees</a>
         <form name="logoutForm" action="logOut.php" method="post">
             <input type="submit" name="logoutButton" value="Log Out" />
