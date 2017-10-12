fcf48c70f54cb00dc98efb28602c19ddad4fce9c
Mahokkit <ma.edward@hotmail.com>
2017-10-12 13:03:17 -0300

    updating for phpstorm

---
+++ Lab4/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>...</title>
+    </head>
+    <body>
+        <form>
+            <fieldset>
+                <legend>Circle</legend>
+                <label>Radius: </label><input type="number" name="radius" />
+            </fieldset>
+
+            <fieldset>
+                <legend>Square</legend>
+                <label>Length: </label><input type="number" name="length" />
+                <label>Width: </label><input type="number" name="Width" />
+            </fieldset>
+
+            <fieldset>
+                <legend>Triangle</legend>
+                <label>Base: </label><input type="number" name="base" />
+                <label>Height: </label><input type="number" name="height" />
+            </fieldset>
+
+            
+        </form>
+
+    </body>
+</html>
