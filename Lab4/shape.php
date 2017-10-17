9f94ad5ec8e39c5bbc5a1e0e5dd6f53caa46cfd4
Mahokkit <ma.edward@hotmail.com>
2017-10-17 18:13:10 -0300

    updating for phpstorm

--- Lab4/shape.php
+++ Lab4/shape.php
@@ -10,6 +10,23 @@
 
     abstract class shape
     {
+        protected $gos;
+        protected $newR;
+
+        /**
+         * @return mixed
+         */
+        public function getGos()
+        {
+            return $this->gos;
+        }
+
         // Abstract methods
         abstract public function CalculateArea();
+
+        public function __construct($in_gos, $in_newR)
+        {
+         $this->gos = $in_gos;
+         $this->newR = $in_newR;
+        }
     }
