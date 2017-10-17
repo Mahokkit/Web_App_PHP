f3c4558534aa28499662010214149a0048c0aaa8
Mahokkit <ma.edward@hotmail.com>
2017-10-17 19:02:33 -0300

    updating for phpstorm

--- Lab4/index.php
+++ Lab4/index.php
@@ -3,14 +3,12 @@
     include_once("square.php");
     include_once("triangle.php");
 
-
-
     $circleArea = new circle($_POST['radius'], $_POST['gos']);
     $squareArea = new square($_POST['length'], $_POST['width']);
     $triangleArea = new triangle($_POST['height'],$_POST['base']);
 
-    $radius = $circleArea->radius;
-    $gos = $_POST['gos'];
+
+
 ?>
 
 <!DOCTYPE html>
@@ -31,35 +29,39 @@
 
             <fieldset>
                 <legend>Square</legend>
-                <label>Length: </label><input type="number" name="length" value="<?php echo $length ?>"/>
-                <label>Width: </label><input type="number" name="width" value="<?php echo $width ?>"/>
+                <label>Length: </label><input type="number" name="length" step="any" value="<?php echo $length ?>"/>
+                <label>Width: </label><input type="number" name="width" step="any" value="<?php echo $width ?>"/>
             </fieldset>
 
             <fieldset>
                 <legend>Triangle</legend>
-                <label>Base: </label><input type="number" name="base" value="<?php echo $base ?>" />
-                <label>Height: </label><input type="number" name="height" value="<?php echo $height ?>"/>
+                <label>Base: </label><input type="number" name="base" step="any" value="<?php echo $base ?>" />
+                <label>Height: </label><input type="number" name="height" step="any" value="<?php echo $height ?>"/>
             </fieldset>
-
-            <p><label>Grow or Shrink By (%): </label><input type="number" name="gos" value="<?php echo $gos ?>" /></p>
 
             <input type="submit" id="insert" value="Calculate" name="Calculate"/>
 
         </form>
 
-        <p> Result </p>
+        <?php
 
-        <h3>Shape: Circle</h3>
-        <p>Area: <?php echo $circleArea->CalculateArea(); ?> </p>
-        <p>Grow or Shrink Area: <?php echo $circleArea->gos(); ?></p>
-        <p>New Radius: <?php echo $circleArea->radius(); ?></p>
+            if(isset($_POST['Calculate']))
+            {
+                echo "  <p> Result </p>
+                        <h3>Shape: Circle</h3>
+                        <p>Area: " . $circleArea->CalculateArea() . "</p>
+                
+                        <h3>Shape: Square</h3>
+                        <p>Area: " . $squareArea->CalculateArea() . "</p>
+                
+                        <h3>Shape: Triangle</h3>
+                        <p>Area: " . $triangleArea->CalculateArea() ."</p>
+                        <p>Grow or Shrink Area: " . $triangleArea->gos() . "</p>
+                      ";
+            }
+        ?>
 
-        <h3>Shape: Square</h3>
-        <p>Area: <?php echo $squareArea->CalculateArea() ?> </p>
 
-        <h3>Shape: Triangle</h3>
-        <p>Area: <?php echo $triangleArea->CalculateArea() ?> </p>
-        <p>Grow or Shrink Area: <?php echo $triangleArea->gos() ?></p>
 
     </body>
 </html>
