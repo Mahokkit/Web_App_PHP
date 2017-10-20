154e09e8b65004bbd372530a5395c81c77427d1d
Mahokkit <ma.edward@hotmail.com>
2017-10-20 16:49:57 -0300

    update from PHPSTOOOOORM

---
+++ Lab5/checkLogin.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+ob_start();
+
+$db = mysqli_connect("localhost", "root","inet2005", "sakila");
+if (!$db)
+{
+    die('Could not connect to test database' . mysqli_connect_error());
+}
+
+if (isset($_POST['loginUser']) && isset($_POST['loginPwd']))
+{
+    $loginUser = $_POST['loginUser'];
+    $loginPwd = $_POST['loginPwd'];
+    $loginUser = stripcslashes($loginUser);
+    $loginPwd = stripcslashes($loginPwd);
+
+    $loginUser = mysqli_real_escape_string($db,$loginUser);
+    $loginPwd = mysqli_real_escape_string($db,$loginPwd);
+    $loginPwd = hash("sha512", $loginPwd);
+    $sql = "SELECT * FROM members WHERE username ='$loginUser'";
+    $sql .= " AND password = '$loginPwd';";
+
+    $result = mysqli_query($db, $sql);
+
+    $count = mysqli_num_rows($result);
+
+    mysqli_close($db);
+
+    if ($count == 1)
+    {
+        $_SESSION['loginUser'] = $loginUser;
+//        HTTP REDIRECT
+        header('location:page1.php');
+    }
+    else
+    {
+        echo "<b> Invalid Login </b><br>";
+        echo "<a href='mainLogin.php'> Try again </a>";
+    }
+}
+
+ob_end_flush();
---
+++ Lab5/form.php
@@ -0,0 +1,72 @@
+<?php
+
+require 'isLoggedIn.php';
+checkIfLoggedIn();
+
+require_once("dbConn.php");
+$conn = getDbConnection();
+
+$TotalRec = mysqli_fetch_array(mysqli_query($conn,"SELECT emp_no FROM employees ORDER BY emp_no DESC LIMIT 1"));
+// get last row ID
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Adding Employees</title>
+    <script src="validation.js" type="text/javascript"></script>
+</head>
+    <body>
+        <p>
+            <b>Insert New Employee</b>
+        </p>
+        <form id="addEmp" name="addEmp" method="post" action="target.php" onsubmit="return validate()">
+            <p>
+                <label>
+                    <input type="hidden" name="empID" id="empID" value="<?php echo $TotalRec[0]+1 ?>" readonly/>
+                </label>
+            </p>
+            <p>
+                <label>Birth Date:
+                    <input type="date" name="empBDate" id="empBDate" value="YYYY-MM-DD"/>
+                    <span id="BD" style="color: red; font-weight: bold"></span>
+                </label>
+            </p>
+            <p>
+                <label>First Name:
+                    <input type="text" name="empFName" id="empFName" />
+                    <span id="FN" style="color: red; font-weight: bold"></span>
+                </label>
+            </p>
+            <p>
+                <label>Last Name:
+                    <input type="text" name="empLName" id="empLName" />
+                    <span id="LN" style="color: red; font-weight: bold;"></span>
+                </label>
+            </p>
+            <p>
+                <label>Gender:
+                    <select name="empGender"required>
+                        <option value="M">Male</option>
+                        <option value="F">Female</option>
+                        <!--<option value="O">Other</option> // cause there's more than one gender!-->
+                    </select>
+                </label>
+            </p>
+            <p>
+                <label>Hired Date:
+                    <input type="date" name="empHDate" id="empHDate" value="YYYY-MM-DD"/>
+                    <span id="HD" style="color: red; font-weight: bold;"></span>
+                </label>
+            </p>
+            <p>
+                <input type="submit" id="insert" value="Add Employee" name="insert"/>
+            </p>
+        </form>
+        <form name="logoutForm" action="logOut.php" method="post">
+            <input type="submit" name="logoutButton" value="Log Out" />
+        </form>
+        <a href="page1.php">Back to Main Menu</a>
+    </body>
+</html>
