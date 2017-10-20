14f14bfc572ccbd79464e51ed28e876560fabce1
Mahokkit <ma.edward@hotmail.com>
2017-10-20 19:39:11 -0300

    update from PHPSTOOOOORM

--- Lab5/checkLogin.php
+++ Lab5/checkLogin.php
@@ -1,40 +1,64 @@
 <?php
+
+
+// INSERT INTO `WebUsers`(user_name,user_pwd,salt) VALUES ('mike',SHA2(CONCAT('saltedmike', 'whatever'),512),'saltedmike')
+
 session_start();
+
 ob_start();
 
 require_once("userConn.php");
 $conn = getDbConnection();
 
-if (isset($_POST['loginUser']) && isset($_POST['loginPwd']))
+$loginUser=$_POST['loginUser'];
+$loginPwd=$_POST['loginPwd'];
+
+$loginUser = stripslashes($loginUser);
+$loginPwd = stripslashes($loginPwd);
+$loginUser = mysqli_real_escape_string($db,$loginUser);
+$loginPwd = mysqli_real_escape_string($db,$loginPwd);
+
+$sqlStatement="SELECT salt FROM WebUsers WHERE user_name='$loginUser'";
+
+$result = mysqli_query($db,$sqlStatement);
+
+$count=mysqli_num_rows($result);
+
+if ($count == 1)
 {
-    $loginUser = $_POST['loginUser'];
-    $loginPwd = $_POST['loginPwd'];
-    $loginUser = stripcslashes($loginUser);
-    $loginPwd = stripcslashes($loginPwd);
+//    	$record = mysqli_fetch_assoc($result);
+//    	$saltedPwd = $record['salt'] . $loginPwd;
+//    	$hashedPwd = hash('sha512',$saltedPwd);
 
-    $loginUser = mysqli_real_escape_string($db,$loginUser);
-    $loginPwd = mysqli_real_escape_string($db,$loginPwd);
-    $loginPwd = hash("sha512", $loginPwd);
-    $sql = "SELECT * FROM members WHERE username ='$loginUser'";
-    $sql .= " AND password = '$loginPwd';";
+    // can also use the PHP crypt() function to hash with a SALT
+    $record = mysqli_fetch_assoc($result);
+    $hashInstructions = '$6$rounds=5000$' . $record['salt'] .'$';
+    $hashedPasswordTemp = crypt($loginPwd, $hashInstructions);
+    $hashedPassword = str_replace ($hashInstructions, "", $hashedPasswordTemp);
+}
 
-    $result = mysqli_query($db, $sql);
 
-    $count = mysqli_num_rows($result);
+$sqlStatement="SELECT * FROM WebUsers WHERE user_name='$loginUser' and user_pwd='$hashedPassword'";
 
-    mysqli_close($db);
+$result = mysqli_query($db,$sqlStatement);
 
-    if ($count == 1)
-    {
-        $_SESSION['loginUser'] = $loginUser;
-//        HTTP REDIRECT
-        header('location:page1.php');
-    }
-    else
-    {
-        echo "<b> Invalid Login </b><br>";
-        echo "<a href='mainLogin.php'> Try again </a>";
-    }
+$count=mysqli_num_rows($result);
+
+mysqli_close($db);
+
+if($count==1)
+{
+    $_SESSION['LoginUser'] = $loginUser;
+    //$_SESSION['LoginPwd'] = $hashedPassword;
+    header("location:page1.php");
+}
+else
+{
+    echo "Wrong Username or Password!";
+    echo "<br/>";
+    echo '<a href="mainLogin.php">Try Again</a>';
 }
 
 ob_end_flush();
+?>
+
