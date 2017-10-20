49e16e0bd627e87ab8a17a1bc186d6749a9e0d43
Mahokkit <ma.edward@hotmail.com>
2017-10-20 19:41:13 -0300

    update from PHPSTOOOOORM

--- Lab5/newUser.php
+++ Lab5/newUser.php
@@ -1,88 +1,85 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title></title>
-    <meta charset="UTF-8" />
-</head>
-<body>
-<header>
-</header>
-<section>
-    <?php
-    require_once('recaptchalib.php');
-    $privatekey = "6LdgRukSAAAAALRpNeav3t26O_M8l2HHKFqN-IIR";
-    $resp = recaptcha_check_answer ($privatekey,
-        $_SERVER["REMOTE_ADDR"],
-        $_POST["recaptcha_challenge_field"],
-        $_POST["recaptcha_response_field"]);
+    <head>
+        <title></title>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
 
-    if (!$resp->is_valid)
-    {
-        // What happens when the CAPTCHA was entered incorrectly
-        echo "<h1>Incorrect Captcha entry.Try again?</h1>";
-        echo "<a href='newUser.html'>Back to New User Form</a>";
-    }
-    else
-    {
-        // Should probably be checking password complexity and match server side as well!!!!
+        <section>
+            <?php
+            require_once('recaptchalib.php');
+            $privatekey = "6LdgRukSAAAAALRpNeav3t26O_M8l2HHKFqN-IIR";
+            $resp = recaptcha_check_answer ($privatekey,
+                $_SERVER["REMOTE_ADDR"],
+                $_POST["recaptcha_challenge_field"],
+                $_POST["recaptcha_response_field"]);
 
-        require_once("userConn.php");
-        $conn = getDbConnection();
+            if (!$resp->is_valid)
+            {
+                // What happens when the CAPTCHA was entered incorrectly
+                echo "<h1>Incorrect Captcha entry.Try again?</h1>";
+                echo "<a href='newUser.html'>Back to New User Form</a>";
+            }
+            else
+            {
+                // Should probably be checking password complexity and match server side as well!!!!
 
-        $loginUser = mysqli_real_escape_string($db,$_POST['loginUser']);
-        $loginPwd = mysqli_real_escape_string($db,$_POST['loginPwd']);
+                require_once("userConn.php");
+                $conn = getDbConnection();
 
-        // create a salt based on the number of seconds since
-        // the Unix Epoch (January 1 1970 00:00:00 GMT).
-        $secondsSinceUnixEpoch = time();
-        $truncatedUserName = substr($loginUser, 0, 16 - strlen($secondsSinceUnixEpoch));
-        //create a 16 character long salt by truncating the username and appending the timestamp to equal
-        // 16 in length
-        $userSalt = $truncatedUserName . $secondsSinceUnixEpoch;
+                $loginUser = mysqli_real_escape_string($db,$_POST['loginUser']);
+                $loginPwd = mysqli_real_escape_string($db,$_POST['loginPwd']);
 
-        // now create the hash using the PHP crypt function
-        // example: crypt('rasmuslerdorf', '$6$rounds=5000$usesomesillystringforsalt$')
-        // the "6" indicates SHA512, the "5000" indicates the rounds of hashing to do
-        $cryptPrefix = '$6$rounds=5000$' . $userSalt .'$';
-        $passwordHashRaw = crypt($loginPwd, $cryptPrefix);
+                // create a salt based on the number of seconds since
+                // the Unix Epoch (January 1 1970 00:00:00 GMT).
+                $secondsSinceUnixEpoch = time();
+                $truncatedUserName = substr($loginUser, 0, 16 - strlen($secondsSinceUnixEpoch));
+                //create a 16 character long salt by truncating the username and appending the timestamp to equal
+                // 16 in length
+                $userSalt = $truncatedUserName . $secondsSinceUnixEpoch;
 
-        //Necessary because I cannot abide storing the HASH type
-        //and rounds of hashing in the hash string
-        $cryptPrefixEscaped = '\$6\$rounds=5000\$' . $userSalt . '\$';
-        $passwordHash = preg_replace('/^' . $cryptPrefixEscaped . '/','',$passwordHashRaw);
+                // now create the hash using the PHP crypt function
+                // example: crypt('rasmuslerdorf', '$6$rounds=5000$usesomesillystringforsalt$')
+                // the "6" indicates SHA512, the "5000" indicates the rounds of hashing to do
+                $cryptPrefix = '$6$rounds=5000$' . $userSalt .'$';
+                $passwordHashRaw = crypt($loginPwd, $cryptPrefix);
 
-        // insert the new user with hash & salt
-        $sqlStatement = "INSERT INTO `WebUsers`(user_name,user_pwd,salt) VALUES ('";
-        $sqlStatement .= $loginUser;
-        $sqlStatement .= "','";
-        $sqlStatement .= $passwordHash;
-        $sqlStatement .= "','";
-        $sqlStatement .= $userSalt;
-        $sqlStatement .= "');";
+                //Necessary because I cannot abide storing the HASH type
+                //and rounds of hashing in the hash string
+                $cryptPrefixEscaped = '\$6\$rounds=5000\$' . $userSalt . '\$';
+                $passwordHash = preg_replace('/^' . $cryptPrefixEscaped . '/','',$passwordHashRaw);
 
-        $result = mysqli_query($db,$sqlStatement);
+                // insert the new user with hash & salt
+                $sqlStatement = "INSERT INTO `WebUsers`(user_name,user_pwd,salt) VALUES ('";
+                $sqlStatement .= $loginUser;
+                $sqlStatement .= "','";
+                $sqlStatement .= $passwordHash;
+                $sqlStatement .= "','";
+                $sqlStatement .= $userSalt;
+                $sqlStatement .= "');";
 
-        if(!$result)
-        {
-            die("Unable to create user: " . mysqli_error($db));
-        }
+                $result = mysqli_query($db,$sqlStatement);
 
-        $count= mysqli_affected_rows($db);
+                if(!$result)
+                {
+                    die("Unable to create user: " . mysqli_error($db));
+                }
 
-        if($count==1)
-        {
-            echo "<h1>The user was added.</h1>";
-            echo "<a href='mainLogin.php'>Go to Login?</a>";
-        }
-        else
-        {
-            echo "<h1>Sorry the user could not be added.</h1>";
-            echo "<a href='newUser.html'>Back to New User Form</a>";
-        }
-    }
-    ?>
-</section>
-<footer>
-</footer>
-</body>
+                $count= mysqli_affected_rows($db);
+
+                if($count==1)
+                {
+                    echo "<h1>The user was added.</h1>";
+                    echo "<a href='mainLogin.php'>Go to Login?</a>";
+                }
+                else
+                {
+                    echo "<h1>Sorry the user could not be added.</h1>";
+                    echo "<a href='newUser.html'>Back to New User Form</a>";
+                }
+            }
+            ?>
+        </section>
+    </body>
 </html>
