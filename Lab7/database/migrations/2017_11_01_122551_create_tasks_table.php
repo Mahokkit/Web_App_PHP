a3e509ab47893a8772721df7f7adca7b6856cdca
Mahokkit <ma.edward@hotmail.com>
2017-11-01 18:14:03 -0300

    following laravcast

--- Lab7/database/migrations/2017_11_01_122551_create_tasks_table.php
+++ Lab7/database/migrations/2017_11_01_122551_create_tasks_table.php
@@ -14,6 +14,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->text('body');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
