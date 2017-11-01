75d4a8bc061c536c887c71edb28748733f0131b5
Mahokkit <ma.edward@hotmail.com>
2017-11-01 17:19:33 -0300

    following laravcast

--- Lab7/routes/web.php
+++ Lab7/routes/web.php
@@ -1,23 +1,16 @@
 <?php
 
-Route::get('/', function () {
+use App\Task;
 
-    $tasks = DB::table('tasks')->get();
-    return view('welcome', compact('tasks'));
-});
-
-Route::get('about', function (){
-    return view('about');
-});
 
 Route::get('/tasks', function () {
-    $tasks = DB::table('tasks')->latest()->get();
+    $tasks = Task::all();
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
 
-    $task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
 
     return view('tasks/show', compact('task'));
 });
