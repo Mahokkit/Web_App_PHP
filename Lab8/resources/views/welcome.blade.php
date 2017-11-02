716e14485958f3256ebe05552cd2a64bfdacb070
Mahokkit <ma.edward@hotmail.com>
2017-11-02 11:49:34 -0300

    following laravcast

---
+++ Lab8/resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+
+
+        <title></title>
+
+    </head>
+    <body>
+    <ul>
+        @foreach ($tasks as $task)
+
+            <li>{{ $task->body }}</li>
+
+        @endforeach
+    </ul>
+    </body>
+</html>
---
+++ Lab8/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Task;
+
+Route::get('/tasks', 'TasksController@index');
+
+
+Route::get('/tasks/{task}', 'TasksController@show');
+
