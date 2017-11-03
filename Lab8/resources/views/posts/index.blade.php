14f627ce7f39f4a35840ab1a3292500bd7eece72
Mahokkit <ma.edward@hotmail.com>
2017-11-03 17:08:01 -0300

    Laracasts From Scratch video 10 to 16
    finished video 13

--- Lab8/resources/views/posts/index.blade.php
+++ Lab8/resources/views/posts/index.blade.php
@@ -4,8 +4,9 @@
 
     <div class="col-sm-8 blog-main">
 
-        @include('posts.post')
-
+        @foreach ($posts as $post)
+            @include('posts.post')
+        @endforeach
         <nav class="blog-pagination">
             <a class="btn btn-outline-primary" href="#">Older</a>
             <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
