327ba7b83471ad0c83623df84d710b71eb13b552
Mahokkit <ma.edward@hotmail.com>
2017-11-08 20:54:36 -0400

    Laracasts From Scratch video 18 to 25
    save point for video 19

--- Lab9/routes/web.php
+++ Lab9/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('/', 'PostsController@index');
+Route::get('/', 'PostsController@index')->name('home');
 
 Route::get('/posts/create', 'PostsController@create');
 
@@ -10,7 +10,9 @@ Route::get('/posts/{post}', 'PostsController@show');
 Route::post('/posts/{post}/comments', 'CommentsController@store');
 
 Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
 Route::get('/login', 'SessionController@create');
+Route::get('/logout', 'RegistrationController@destroy');
 // controller => PostsController
 
 // Eloquent model => Post
