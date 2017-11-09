00e0c12e93bfacd0074efbcbd0143eaa6d7eb8ba
Mahokkit <ma.edward@hotmail.com>
2017-11-09 08:14:26 -0400

    Laracasts From Scratch video 18 to 25
    finished on video 21

--- Lab9/app/Http/Controllers/PostsController.php
+++ Lab9/app/Http/Controllers/PostsController.php
@@ -33,20 +33,15 @@ class PostsController extends Controller
 
         $posts = $posts->get();
 
-        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-            ->groupBy('year', 'month')
-            ->orderByRaw('min(created_at) desc')
-            ->get()
-            ->toArray();
 
-
-        return view('posts.index', compact('posts','archives'));
+        return view('posts.index', compact('posts'));
     }
 
     public function show(Post $post)
     {
 
-        return view('posts.show', compact('post','archives'));
+
+        return view('posts.show', compact('post'));
     }
 
     public function create()
