bb89ac29ffbdd4c9fc193fb5422188555d3da8e3
Mahokkit <ma.edward@hotmail.com>
2017-11-09 19:47:47 -0400

    Laracasts From Scratch video 26 to 32
    finish video 27

--- Lab10/resources/views/emails/welcome-again.blade.php
+++ Lab10/resources/views/emails/welcome-again.blade.php
@@ -4,7 +4,7 @@
 Thanks so much for registering!
 
 @component('mail::button', ['url' => 'https//laracasts.com'])
-Button Text
+Start Browsing
 @endcomponent
 
 @component('mail::panel', ['url' => ''])
