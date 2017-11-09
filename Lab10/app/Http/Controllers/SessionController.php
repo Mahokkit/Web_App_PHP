690c39e67cc5c95dd3703f27442e13dc5d961d95
Mahokkit <ma.edward@hotmail.com>
2017-11-09 19:00:09 -0400

    update from PHPSTORM

---
+++ Lab10/app/Http/Controllers/SessionController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'destroy']);
+    }
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        //attempt to auth user
+
+        if (! auth()->attempt(\request(['email', 'password'])))
+        {
+            return back()->withErrors([
+                'message' => 'Please check credentials and try again'
+            ]);
+        };
+
+        return redirect()->home();
+
+        // if not redirect back
+
+        // if so, sign them in
+
+        // redirect
+    }
+
+    public function destroy()
+    {
+        auth()->logout();
+
+        return redirect()->home();
+    }
+}
---
+++ Lab10/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+
+//dd(resolve('App\Billing\Stripe'));
+
+Route::get('/', 'PostsController@index')->name('home');
+
+Route::get('/posts/create', 'PostsController@create');
+
+Route::post('/posts', 'PostsController@store');
+Route::get('/posts/{post}', 'PostsController@show');
+
+Route::post('/posts/{post}/comments', 'CommentsController@store');
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
+
+Route::get('/login', 'SessionController@create');
+Route::post('/login', 'SessionController@store');
+Route::get('/logout', 'SessionController@destroy');
+// controller => PostsController
+
+// Eloquent model => Post
+
+// migration => create_posts_table
