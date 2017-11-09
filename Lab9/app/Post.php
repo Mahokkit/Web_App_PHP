cee612902d5b19b2f8a2378238799f9622a1249f
Mahokkit <ma.edward@hotmail.com>
2017-11-09 01:09:13 -0400

    Laracasts From Scratch video 18 to 25
    save point on video 20
    
    stuck.

--- Lab9/app/Post.php
+++ Lab9/app/Post.php
@@ -23,17 +23,18 @@ class Post extends Model
 
     }
 
-    public function scopeFilter($query, $filters)
-    {
-        if ($month = $filters('month'))
-        {
-            $query->whereMonth('created_at', Carbon::parse($month)->month);
-        }
-
-        if ($year = $filters('year'))
-        {
-            $query->whereYear('created_at', $year);
-        }
-
-    }
+//    public function scopeFilter($query, $filters)
+//    {
+//
+//        if ($month = $filters('month'))
+//        {
+//            $query->whereMonth('created_at', Carbon::parse($month)->month);
+//        }
+//
+//        if ($year = $filters('year'))
+//        {
+//            $query->whereYear('created_at', $year);
+//        }
+//
+//    }
 }
