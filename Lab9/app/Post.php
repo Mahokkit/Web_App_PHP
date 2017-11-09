cc264ff67018abc53c8db31dd3ffbd1fb2f038d6
Mahokkit <ma.edward@hotmail.com>
2017-11-09 00:59:47 -0400

    Laracasts From Scratch video 18 to 25
    save point on video 20

--- Lab9/app/Post.php
+++ Lab9/app/Post.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -19,6 +20,20 @@ class Post extends Model
     public function addComment($body)
     {
         $this->comments()->create(compact('body'));
+
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        if ($month = $filters('month'))
+        {
+            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if ($year = $filters('year'))
+        {
+            $query->whereYear('created_at', $year);
+        }
 
     }
 }
